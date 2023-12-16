e243ba3cf962e38b3a117851cd1d514b693cecc6
Rohit Choudhary <rohit4444choudhary@gmail.com>
2023-12-16 21:39:52 +0530

    one with the linr error

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,9 @@ import express from 'express';
 
 const app = express();
 const port = 3000;
-
+const asd ='Asdsad';
 app.get('/', (req, res) => {
-    res.send('Hello From type world. Learning Github actions....asdsd');
+    res.send('Hello From type world. Learning Github actions....');
 });
 
 app.listen(port, () => {
