289c53f9e5e7c8074df7b991625866775c4e78e8
rohit-2 <rohit4444choudhary@gmail.com>
2023-12-22 00:42:50 +0530

    check git branch setting

--- src/server.ts
+++ src/server.ts
@@ -7,5 +7,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Server running from docker. http://localhost:${port}/`);
+    console.log(`Server running from docker http://localhost:${port}/`);
 });
